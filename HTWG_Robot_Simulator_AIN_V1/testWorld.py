9c09b7e858c36f4a0aa0eee6b5b539633ae34b4b
NicoLutz <ni431lut@htwg-konstanz.de>
2017-12-12 09:53:26 +0100

    Added testWorld.

---
+++ HTWG_Robot_Simulator_AIN_V1/testWorld.py
@@ -0,0 +1,9 @@
+from HTWG_Robot_Simulator_AIN_V1.World import *
+
+def buildWorld():
+    world = World(20, 10)
+
+    world.addLine(1, 5, 10, 5)
+   # world.addLine(10, 5, 10, 1)
+
+    return world
